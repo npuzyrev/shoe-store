8571c44c6d350db34d16028e4b89613d83c0fd77
nikolay.puzyrev <nikolay.puzyrev@team.wrike.com>
2018-05-03 02:56:51 +0300

    passed the first test (ProductList)

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function ProductList(props) {
     return (
-        <div>ProductList</div>
+        <ul>
+            {
+                props.products.map(product => (
+                    <li key={product.id}>
+                        {product.name}
+                    </li>
+                ))
+            }
+        </ul>
     )
+}
+
+ProductList.propTypes = {
+    products: PropTypes.array.isRequired
 }
 
 export default ProductList;

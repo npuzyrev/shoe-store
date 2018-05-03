76d089b052440ec8a83d6479dae9b90d78ae11b4
nikolay.puzyrev <nikolay.puzyrev@team.wrike.com>
2018-05-03 03:15:32 +0300

    refactor test of ProductList

--- src/components/ProductList/ProductList.test.js
+++ src/components/ProductList/ProductList.test.js
@@ -2,35 +2,29 @@ import React from 'react';
 import {shallow} from 'enzyme'
 import ProductList from './index';
 
-it('should render a list of products as an unordered list', () => {
-    const mockProducts = [
+let mockProducts, wrapper;
+
+beforeEach(() => {
+    mockProducts = [
         {id: 1, name: 'Mock Product 1', brand: 'MockBrandA'},
         {id: 2, name: 'Mock Product 2', brand: 'MockBrandB'},
         {id: 3, name: 'Mock Product 3', brand: 'MockBrandC'},
     ];
-    const wrapper = shallow(<ProductList products={mockProducts} />);
+    wrapper = shallow(<ProductList products={mockProducts} />);
+});
+
+
+it('should render a list of products as an unordered list', () => {
     expect(wrapper.find('li').length).toEqual(mockProducts.length);
 });
 
 it('should display the product name in each `<li>` element', () => {
-    const mockProducts = [
-        {id: 1, name: 'Mock Product 1', brand: 'MockBrandA'},
-        {id: 2, name: 'Mock Product 2', brand: 'MockBrandB'},
-        {id: 3, name: 'Mock Product 3', brand: 'MockBrandC'},
-    ];
-    const wrapper = shallow(<ProductList products={mockProducts} />);
     const firstElement = wrapper.find('li').first();
     // Check that the product name is contained somewhere in this element
     expect(firstElement.contains(mockProducts[0].name)).toEqual(true);
 });
 
 it('should display the brand name in each `<li>` element', () => {
-    const mockProducts = [
-        {id: 1, name: 'Mock Product 1', brand: 'MockBrandA'},
-        {id: 2, name: 'Mock Product 2', brand: 'MockBrandB'},
-        {id: 3, name: 'Mock Product 3', brand: 'MockBrandC'},
-    ];
-    const wrapper = shallow(<ProductList products={mockProducts} />);
     const firstElement = wrapper.find('li').first();
     // Check that the brand name is contained somewhere in this element
     expect(firstElement.contains(mockProducts[0].brand)).toEqual(true);
